ebc3d07855e14b2171941eecb9e6eb7ef7aea247
ytt1218 <ytt1218@gmail.com>
2022-08-25 10:34:16 -0700

    added initial project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ViewComponent } from './components/products/view/view.component';
+import { WishlistComponent } from './components/products/wishlist/wishlist.component';
+import { CartComponent } from './components/products/cart/cart.component';
+import { OrdersComponent } from './components/orders/orders.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { ProfileComponent } from './components/auth/profile/profile.component';
+import { ChangeComponent } from './components/auth/change/change.component';
+import { ForgetPasswordComponent } from './components/auth/forget-password/forget-password.component';
+import { CheckoutComponent } from './components/products/checkout/checkout.component';
+import { ProductsModule } from './components/products/products.module';
+import { AuthModule } from './components/auth/auth.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    NavbarComponent,
+    NotfoundComponent,
+   
+    OrdersComponent,
+ 
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ProductsModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
